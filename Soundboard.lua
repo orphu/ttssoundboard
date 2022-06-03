960044a5b15edd62ba1d32d7353bcbaaaee1db50
Tyler Lund <github@bubblemod.org>
2022-06-02 20:58:13 -0400

    Update Soundboard.lua

--- Soundboard.lua
+++ Soundboard.lua
@@ -53,7 +53,7 @@ end
 
 function nicehit()
     playRandom(
-        "https://tartarus.briareos.net/tts/dd/",
+        "https://tartarus.briareos.net/demo/dd/",
         {
             "vo_narr_good_crit_01 {176e47e9-86d0-4a3a-87c8-3069896c8320}.ogg",
             "vo_narr_good_crit_02 {3d31156c-2c31-4aea-a391-9294107a5f71}.ogg",
@@ -66,7 +66,7 @@ end
 
 function nicekill()
     playRandom(
-        "https://tartarus.briareos.net/tts/dd/",
+        "https://tartarus.briareos.net/demo/dd/",
         {
             "vo_narr_good_kill_big_01 {975d4411-f757-4392-ad67-09b29402208a}.ogg",
             "vo_narr_good_kill_big_02 {058b76d0-3a7a-4b35-b2d8-b4494cad0653}.ogg",
@@ -90,7 +90,7 @@ end
 
 function wound()
     playRandom(
-        "https://tartarus.briareos.net/tts/dd/",
+        "https://tartarus.briareos.net/demo/dd/",
         {
             "vo_narr_good_kill_dot_01 {fb40381e-cb31-4ef9-a3ca-0ce024927bc0}.ogg",
             "vo_narr_good_kill_dot_02 {739be454-4b4c-4f27-9935-458d0d980389}.ogg",
@@ -100,7 +100,7 @@ end
 
 function trap()
     playRandom(
-        "https://tartarus.briareos.net/tts/dd/",
+        "https://tartarus.briareos.net/demo/dd/",
         {
             "vo_narr_bad_trap_01 {5dbf2c2a-a9d8-4262-a77e-89459c91c406}.ogg",
             "vo_narr_bad_trap_02 {0e252056-4f06-4c47-b95a-dc90b7543be7}.ogg",
@@ -112,7 +112,7 @@ end
 
 function loot()
     playRandom(
-        "https://tartarus.briareos.net/tts/dd/",
+        "https://tartarus.briareos.net/demo/dd/",
         {
             "vo_narr_good_lootfirst_01 {fc00b547-142f-40b5-8a64-a7489a6ed200}.ogg",
             "vo_narr_good_lootfirst_02 {a4c464b0-ed1e-456f-8dd7-695db93a494a}.ogg",
@@ -125,7 +125,7 @@ end
 
 function badhit()
     playRandom(
-        "https://tartarus.briareos.net/tts/dd/",
+        "https://tartarus.briareos.net/demo/dd/",
         {
             "vo_narr_bad_crit_01 {2b22ec53-1a49-4b5b-a2b1-0cc4778dd9f4}.ogg",
             "vo_narr_bad_crit_02 {39b52bee-741d-4876-8b9e-63289f300181}.ogg",
@@ -143,7 +143,7 @@ end
 
 function lol()
     playRandom(
-        "https://tartarus.briareos.net/tts/",
+        "https://tartarus.briareos.net/demo/",
         {
             "sitcom-laughing-1.mp3",
         },
@@ -153,7 +153,7 @@ end
 
 function trombone()
     playRandom(
-        "https://tartarus.briareos.net/tts/trombone/",
+        "https://tartarus.briareos.net/demo/trombone/",
         {
             "sadtrombone2.mp3",
             --"sadtrombone3.mp3"
@@ -172,121 +172,13 @@ function loadMusic()
     MusicPlayer.setPlaylist(
         {
             {
-            url = "https://tartarus.briareos.net/tts/hades/01%20No%20Escape.mp3",
-            title = "No Escape"
+            url = "https://tartarus.briareos.net/demo/music/Lyonesse.ogg",
+            title = "Lyonesse"
             },
             {
-            url = "https://tartarus.briareos.net/tts/hades/02%20The%20House%20of%20Hades.mp3",
-            title = "The House of Hades"
+            url = "https://tartarus.briareos.net/demo/music/Spring%20Village.ogg",
+            title = "Village"
             },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/03%20Out%20of%20Tartarus.mp3",
-            title = "Out of Tartarus"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/04%20Wretched%20Shades.mp3",
-            title = "Wretched Shades"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/06%20The%20Painful%20Way.mp3",
-            title = "The Painful Way"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/07%20From%20Olympus.mp3",
-            title = "From Olympus"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/08%20Through%20Asphodel.mp3",
-            title = "Through Asphodel"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/10%20Final%20Expense.mp3",
-            title = "Final Expense"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/11%20Mouth%20of%20Styx.mp3",
-            title = "Mouth of Styx"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/12%20Primordial%20Chaos.mp3",
-            title = "Primordial Chaos"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/13%20The%20Bloodless.mp3",
-            title = "The Bloodless"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/14%20Scourge%20of%20the%20Furies.mp3",
-            title = "Scourge of the Furies"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/16%20Field%20of%20Souls.mp3",
-            title = "Field of Souls"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/18%20Death%20and%20I.mp3",
-            title = "Death and I"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/19%20Rage%20of%20the%20Myrmidons.mp3",
-            title = "Rage of the Myrmidons"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/20%20The%20Exalted.mp3",
-            title = "The Exalted"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/21%20Last%20Words.mp3",
-            title = "Last Words"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/22%20The%20King%20and%20the%20Bull.mp3",
-            title = "The King and the Bull"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/24%20Gates%20of%20Hell.mp3",
-            title = "Gates of Hell"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/25%20God%20of%20the%20Dead.mp3",
-            title = "God of the Dead"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/27%20On%20the%20Coast.mp3",
-            title = "On The Coast"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/28%20In%20the%20Blood%20(ft.%20Ashley%20Barrett).mp3",
-            title = "In The Blood"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/hades/29%20The%20Unseen%20Ones%20(ft.%20Masahiro%20Aoki%20and%20Daisuke%20Kurosawa).mp3",
-            title = "The Unseen Ones"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/gow/01%20-%20God%20of%20War.mp3",
-            title = "God of War"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/gow/06%20-%20Peaks%20Pass.mp3",
-            title = "Peaks Pass"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/gow/08%20-%20The%20Dragon.mp3",
-            title = "The Dragon"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/gow/10%20-%20Magni%20and%20Modi.mp3",
-            title = "Magni and Modi"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/gow/15%20-%20Stone%20Mason.mp3",
-            title = "Stone Mason"
-            },
-            {
-            url = "https://tartarus.briareos.net/tts/gow/16%20-%20Valkyries.mp3",
-            title = "Valkyries"
-            }     
         }
     )
     MusicPlayer.playlist_index = math.random(#MusicPlayer.getPlaylist())
